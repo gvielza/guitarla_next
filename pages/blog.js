dce2332e8d7eb16df93e42e2490f3cddb187a612
Gionnelly <80008338+gvielza@users.noreply.github.com>
2022-05-05 12:12:08 -0300

    agregando notas

--- pages/blog.js
+++ pages/blog.js
@@ -1,15 +1,26 @@
 
 
 import Layout from '../components/Layout';
-
+ 
 const Blog = () => {
-    return (
-        <Layout
-        pagina='Blog'>
-            <h1>Desde Blog</h1>
-           
-        </Layout>
-    );
-};
-
-export default Blog;
+ 
+  return (
+    <Layout pagina="Blog">
+        <h1>Desde Blog</h1>
+    </Layout>
+  )
+}
+ 
+export async function getServerSideProps() {
+ 
+  const url = 'http://localhost:1337/blogs';
+  const respuesta = await fetch(url);
+  const entradas = await respuesta.json();
+  console.log(entradas);
+ 
+  return {
+    props: {
+      entradas
+    }
+  }
+}
